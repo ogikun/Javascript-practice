d7fffd8096993795eebbbe00be5a408229c3617c
Ogino Kazuya <Ogino@ogiMacBook-Pro.local>
2021-01-12 18:43:36 +0900

    js-2

---
+++ 2/app.js
@@ -0,0 +1,59 @@
+// var hello = 'Hello World';
+// alert(hello);
+// alert(4 + 3);
+// alert(4 % 3)
+// alert('Hello' + 'World!')
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
+
+// // 条件分岐
+
+// var orange = 100;
+// var apple = 120;
+// if(orange < apple){
+//   alert('みかんの値段がリンゴより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごは同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い')
+// }
+
+// // 繰り返し処理
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// // 回数指定処理
+
+// var i;
+// var num = 0;
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足した結果は' + num + 'です');
+
+// 確認問題
+
+var ans = 10;
+
+var ans2 = ans + 10;
+alert(ans + "+" + 10 + "="+ ans2);
+
+var ans3 = ans2 - 10;
+alert(ans2 + "-" + 10 + "=" + ans3);
+
+var ans4 = ans3 * 10;
+alert(ans3 + "*" + 10 + "=" + ans4);
+
+var ans5 = ans4 / 10;
+alert(ans4 + "/" + 10 + "=" + ans5);
